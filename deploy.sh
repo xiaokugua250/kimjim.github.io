ed12ba801df08602c4a8d1cad84f9d68a51da6e2
duliang <liang.du@nscc-gz.cn>
2020-12-12 12:41:56 +0800

    add code

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,12 @@
  git add .
  git commit -m "modify site"
  git checkout gh-pages
+ rm -r *
+
+git checkout main public\**
+mv public\* .
+rm -r public
+ git add .
+ git commit -m "modify site"
+ git push
  
